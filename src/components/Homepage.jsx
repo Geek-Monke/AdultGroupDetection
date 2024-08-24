36b862a016657cb8c88bf6d8903c70d6311616e8
subhraneel2005 <digitalsubhraneel@gmail.com>
2024-08-24 18:57:09 +0530

    logic update #1

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,19 +1,16 @@
 'use client'
 
 import React from 'react'
-import { useSession, signOut, signIn } from 'next-auth/react';
 import { Input } from './ui/input';
 import { Button } from './ui/button';
 import { useRouter } from 'next/navigation';
 
 function Homepage() {
 
-    const session = useSession();
-    const user = session?.data?.user;
     const router = useRouter();
 
     const signInHandler = () => {
-        router.push('/api/auth/signin')
+        alert('Function not made yet')
       }
     
 
@@ -24,16 +21,16 @@ function Homepage() {
         <h1 className='text-[3rem] text-center'>Welcome to <span className='text-red-600'>Adult</span> Content Blocker</h1>
         
         <div className='max-w-xs w-full mt-16'>
-        {user ? 
-            <div className='flex flex-col space-y-5'>
+       
+            {/* <div className='flex flex-col space-y-5'>
                 <Input placeholder="Enter telegram group name"/>
                 <div className='max-w-xs w-full flex justify-center items-center'>
                 <Button>Report</Button>
                 </div>
-            </div>
-        : <div className='max-w-xs w-full flex justify-center items-center'>
+            </div> */}
+        <div className='max-w-xs w-full flex justify-center items-center text-[16px]'>
             <Button onClick={signInHandler}>Sign In</Button>    
-        </div>}
+        </div>
         </div>
     </div>
   )
